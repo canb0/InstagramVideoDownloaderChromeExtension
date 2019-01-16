fad0e0fb50cec08098c464763d7306103325bd89
canberk@getqardio.com <Gulin-2013>
2019-01-16 14:42:26 +0100

    Refactored folder stucuture

---
+++ README.md
@@ -0,0 +1,5 @@
+This plugin allows yo to download an Instagram Video.
+- Plugin Works only in the specific post's page.
+- In case you are in the instagram's infinite scroll page, it will alert the user to go to specific post's page
+
+This is an experimental plug-in. Hope to implement the same approach in different web sites.
--- scripts/download.js
+++ scripts/download.js
@@ -4,19 +4,19 @@ document.getElementById("download").addEventListener('click', () => {
     function downloadVideo() {
         let url = document.URL;
         console.log('Page URL '+url.toString());
-        if(!url.toString().includes('www.instagram.com/p/')){
+        if(!url.includes('instagram'))
+            alert('You are not even in instagram web site');
+        else if(!url.toString().includes('www.instagram.com/p/')){
             alert('You are in the main page. Go to specific video\'s page by "Go To Post" menu and download the video.');
         }else{
             let download_link = document.querySelectorAll('video.tWeCl')[0].getAttribute('src');
             console.log('DOWNLOAD URL is'+download_link);
-            var pass_array=[];
-            pass_array.push(download_link);
-            var param = {collection : pass_array};
+            var arguments=[];
+            arguments.push(download_link);
+            var param = {collection : arguments};
             chrome.runtime.sendMessage(param);
-
         }
     }
-
     //We have permission to access the activeTab, so we can call chrome.tabs.executeScript:
     chrome.tabs.executeScript({
         code: '(' + downloadVideo + ')();'
